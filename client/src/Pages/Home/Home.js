8b3e21ea594b64eec939b748f584036cabb82ed1
Brandon Haines <haines.brandon.91@gmail.com>
2018-04-02 11:44:48 -0700

    updated styling and getting articles now works

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import "./Home.css";
 import Panel from '../../Components/Panel';
 import API from '../../utils/API';
+import {SaveBtn} from "../../Components/Buttons";
+
 
 class Home extends Component {
   state = {
@@ -31,6 +34,12 @@ class Home extends Component {
     
   }
 
+  saveArticle = article => {
+    API.saveArticle({title: article.headline.main, url: article.web_url})
+    .then(alert('Article saved!'))
+    .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -38,7 +47,7 @@ class Home extends Component {
           <div className="input-group">
             <h4 className="text-center">Topic</h4>
             <input 
-              className="input form-control" 
+              className="form-control" 
               value={this.state.q}
               name="q"
               onChange={this.handleChange}
@@ -47,7 +56,7 @@ class Home extends Component {
           <div className="input-group">
             <h4 className="text-center">Start Year</h4>
             <input 
-              className="input form-control" 
+              className="form-control" 
               value={this.state.start_year}
               name="start_year"
               onChange={this.handleChange}
@@ -56,21 +65,28 @@ class Home extends Component {
           <div className="input-group">
             <h4 className="text-center">End Year</h4>
             <input 
-              className="input form-control" 
+              className="form-control" 
               value={this.state.end_year}
               name="end_year"
               onChange={this.handleChange}
             />
           </div>
-          <div className="input-group">
+          <div className="searchBtn text-center">
             <button 
-            className="btn btn-primary "
+            className="btn btn-primary"
             onClick={this.handleSearch}
             >Search</button>
           </div>
         </Panel>
         <Panel title={"Results"}>
-          <div>inside results</div>
+          <div className="ArticleResults">
+            {this.state.articles.map(article => (
+              <div key={article._id}>
+                <a href={article.web_url} target="_blank">{article.headline.main}</a>
+                <SaveBtn onClick={() => this.saveArticle(article)} />
+              </div>
+            ))}
+          </div>
         </Panel>
       </div>
     );
--- client/src/Pages/Saved/Saved.js
+++ client/src/Pages/Saved/Saved.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import Panel from '../../Components/Panel';
+import {DeleteBtn} from "../../Components/Buttons";
+import API from "../../utils/API";
+
 class Saved extends Component {
   state = {
+    saved: []
+  };
 
+  componentDidMount() {
+    this.getArticles();
+  }
+
+  getArticles = () => {
+    API.getSavedArticles()
+      .then(res => this.setState({ saved: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  deleteArticle = id => {
+    API.deleteArticle(id)
+      .then(res => this.loadArticles())
+      .catch(err => console.log(err));
   };
 
   render() {
     return (
       <div className="container-fluid">
         <Panel title="Saved">
-          <div>inside saved</div>
+          <div>
+          {this.state.saved.map(article => (
+            <div key={article._id}>
+              <a href={article.url}>{article.title}</a>
+              <DeleteBtn onClick={() => this.deleteArticle(article._id)} />
+            </div>
+          ))}
+          </div>
         </Panel>
       </div>
     );
